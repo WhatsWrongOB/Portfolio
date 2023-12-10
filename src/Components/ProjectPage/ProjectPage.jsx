009ef32a55ea35d21f28044f9d93e5122ab1839a
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2023-12-10 18:17:11 +0500

    first commit

--- src/Components/ProjectPage/ProjectPage.jsx
+++ src/Components/ProjectPage/ProjectPage.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import './ProjectPage.css'
 import ProjectCard from '../Projects/ProjectCard'
+import { useState } from 'react'
 
 const Skills = ({ projects }) => {
+
+  const [search, setSearch] = useState('')
+
+  const filteredProjects = projects.filter((item) => {
+    const check = item.name.toLowerCase().includes(search.toLowerCase());
+    return check;
+  });
+
+
   return (
     <section className="project_page">
       <h1 className="project_page_heading">Pro <span>jects</span>:-</h1>
+      <input type="search"
+        className='search'
+        placeholder='search projets'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <div className="project_card_container">
         {
-          projects.map((item) => (
+          filteredProjects.map((item) => (
             <ProjectCard key={item.id} name={item.name} description={item.description} img={item.image} url={item.url} />
           ))
         }
