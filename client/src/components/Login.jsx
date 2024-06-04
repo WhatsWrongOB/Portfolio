f2c383735fcba6b9b92933a068b84c19e182211d
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2024-06-04 19:55:21 +0500

    change form design

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,73 +1,98 @@
-import React, { useState } from "react";
-import { FaUser, FaKey } from "react-icons/fa";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import { FaUser, FaKey, FaUserAstronaut } from "react-icons/fa";
+import user from "/public/user.svg";
 import { Link, useNavigate } from "react-router-dom";
-import { useStore } from "../Context";
 import { toast } from "react-hot-toast";
 import ClipLoader from "react-spinners/ClipLoader";
+import { useStore } from "../Context";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const { login, loading } = useStore();
   const navigate = useNavigate();
+  const [active, setActive] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleLogin = async (e) => {
+  const handleShowPass = () => {
+    setActive(!active);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const { email, password } = formData;
+
     try {
-      const success = await login(email, password);
-      if (success) {
+      const res = await login(email, password);
+      if (res) {
+        toast.success("Login Successful");
         navigate("/dashboard");
-        toast.success("Login Successfully");
-      } else {
-        toast.error("Incorrect email or password");
       }
     } catch (error) {
-      console.log(error);
+      toast.error(error.message);
     }
   };
 
   return (
-    <section className="login">
-      <div className="form">
-        <div className="login_upper">
-          <h1>Admin Login</h1>
+    <section className="auth">
+      <div className="auth_container">
+      <div className="auth_upper">
+        <div>
+          <img src={user} alt="user" />
         </div>
-        <div className="login_bottom">
-          <form className="login_form" onSubmit={handleLogin}>
-            <div className="user_email">
-              <FaUser color="black" size={19} />
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="user_pass">
-              <FaKey color="black" size={19} />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <button type="submit" className="login_btn">
-              {loading ? (
-                <ClipLoader loading={loading} size={15} color="white" />
-              ) : (
-                "Login"
-              )}
-            </button>
-          </form>
-        </div>
-        <p className="link_home">
-          Back to home? <Link to="/">Home</Link>
-        </p>
+        <h1>Admin Login</h1>
       </div>
+      <form className="auth_form" onSubmit={handleSubmit}>
+        <div className="group">
+          <FaUserAstronaut size={17} color="black"/>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="group">
+          <FaKey size={17} color="black"/>
+          <input
+            type={active ? "text" : "password"}
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <span className="eye" onClick={handleShowPass}>
+            {active ? <FaEyeSlash color="black"/> : <FaEye color="black"/>}
+          </span>
+        </div>
+
+        <button type="submit">
+          {loading ? (
+            <ClipLoader loading={loading} size={17} color="white" />
+          ) : (
+            "Login"
+          )}
+        </button>
+      </form>
+      <p className="forget">
+        Back to home ? <Link to="/">click here</Link>
+      </p>
+    </div>
     </section>
   );
 };
