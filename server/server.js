83a0219bdeeee030a83c14fb4d6fdff3a4b6699a
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2024-06-07 14:15:06 +0500

    fix import

--- server/server.js
+++ server/server.js
@@ -8,42 +8,50 @@ import { userRouter } from "./routes/auth.js";
 import { projectRouter } from "./routes/project.js";
 import { contactRouter } from "./routes/contact.js";
 
+dotenv.config({ path: "./config/config.env" });
 
 export const myCache = new NodeCache();
 const app = express();
 
-dotenv.config({ path: "./config/config.env" });
-
+// CORS configuration
 const corsOptions = {
-  origin: "*",
+  origin: "*", 
   methods: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
   credentials: true,
 };
-
 app.use(cors(corsOptions));
+
+
+// Middleware
 app.use(cookieParser());
 app.use(express.json());
 
+
+// Routes
 app.use("/auth", userRouter);
 app.use("/project", projectRouter);
 app.use("/contact", contactRouter);
 
+
+// Default route
 app.use("/", (req, res) => {
   res.send("Server working");
 });
 
-const PORT = process.env.PORT || 3000;
-
-connectDatabase().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server Running on the Port http://localhost:${PORT}/`);
-  });
-});
-
+// Error handling middleware
 app.use((err, req, res, next) => {
   if (err instanceof Error && err.message.includes("Not allowed by CORS")) {
     res.status(403).json({ message: "CORS error: Access denied" });
   } else {
     next(err);
   }
+});
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to database and start server
+connectDatabase().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server Running on Port http://localhost:${PORT}/`);
+  });
 });
